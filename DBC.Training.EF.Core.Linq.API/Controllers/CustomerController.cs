a92cf7e54509a7df80f1dd0d11acdb9418eb78d5
Chris Cabezudo <christopher.cabezudo@devblockcloud.com>
2020-02-15 21:42:37 -0600

    Update

---
+++ DBC.Training.EF.Core.Linq.API/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBC.Training.EF.Core.Linq.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBC.Training.EF.Core.Linq.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private DataRepository _dataRepository;
+        public CustomerController(DataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        [HttpGet("GetAll")]
+        public List<Customer> Get()
+        {
+            _dataRepository.InitCustomer();
+            
+            return _dataRepository.GetAllCustomers();
+        }
+    }
+}
